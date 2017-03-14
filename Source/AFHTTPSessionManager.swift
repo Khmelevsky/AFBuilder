d773eb575f7705aa9a9addeb45acd05cf95d3d57
Alexander Khmelevsky <khmelevsky.alex@gmail.com>
2017-03-14 09:40:50 +0300

    Method public

--- Source/AFHTTPSessionManager.swift
+++ Source/AFHTTPSessionManager.swift
@@ -10,19 +10,6 @@ import AFNetworking
 
 extension AFHTTPSessionManager {
     
-    public enum Method {
-        case head
-        case get
-        case post
-        case put
-        case delete
-        case patch
-        case multipartFormData(((AFMultipartFormData) -> Swift.Void)?)
-        
-        @available(*, deprecated, message: "Use multipartFormData")
-        case postMultipartForm(((AFMultipartFormData) -> Swift.Void)?)
-    }
-    
     public func builder(withUrlString url: String, clouser:(Configurator) -> Swift.Void) -> RequestBuilder {
         let configurator = AFHTTPDefaultConfigurator(urlString: url, manager:self)
         clouser(configurator)
--- Source/Configurator.swift
+++ Source/Configurator.swift
@@ -11,18 +11,37 @@ import AFNetworking
 @available(*, renamed:"Configurator", message: "Use Configurator")
 public typealias AFBuildConfigurator = Configurator
 
+
+public enum Method {
+    case head
+    case get
+    case post
+    case put
+    case delete
+    case patch
+    case multipartFormData(((AFMultipartFormData) -> Swift.Void)?)
+    
+    @available(*, deprecated, message: "Use multipartFormData")
+    case postMultipartForm(((AFMultipartFormData) -> Swift.Void)?)
+}
+
+
 public protocol Configurator: class {
+    
     var urlString: String { get set }
-    var method: AFHTTPSessionManager.Method { get set }
+    var method: Method { get set }
     var params: Any? { get set }
     var handlers: [HandlerProtocol] { get set }
     var manager: AFHTTPSessionManager { get set }
+    
 }
+
+
 
 open class AFHTTPDefaultConfigurator: Configurator {
     
     open var urlString: String
-    open var method: AFHTTPSessionManager.Method = .get
+    open var method: Method = .get
     open var params: Any? = nil
     open var handlers = [HandlerProtocol]()
     open var manager: AFHTTPSessionManager
